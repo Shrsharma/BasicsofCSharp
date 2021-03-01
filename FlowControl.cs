9c9eca41f7ceb74c8246e1313710d827f44450be
Shrsharma <shrsharma@teksystems.com>
2021-03-01 16:40:50 +0530

    Changes in FlowControl File

---
+++ FlowControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsofCSharp
+{
+    class FlowControl
+    {
+
+        public static void Do()
+        {
+            int a = 150;
+            if (a==100)
+            {
+                Console.WriteLine("The value is 100");
+            }
+            else
+            {
+                Console.WriteLine("The value is {0}", a);
+            }
+
+            //While Loop
+            Console.WriteLine("\nWhile Loop:");
+            var counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            //For Loop
+            Console.WriteLine("\nFor Loop:");
+            for (int i = counter; i < 20; i++)
+            {
+                counter = i;
+                Console.WriteLine(counter);
+            }
+
+            //Do-While Loop
+            Console.WriteLine("\nDo-While Loop:");
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+
+            } while (counter < 30);
+
+            //For Each
+
+            int[] array = { 10, 20, 30, 40, 50 };
+            Console.WriteLine("\nFor each loop");
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace BasicsofCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            PrimitiveTypes.Do();
+            //PrimitiveTypes.Do();
+            FlowControl.Do();
         }
     }
 }
